0e1006e791d19809474ba48c980463d85ba1b552
Haripriya987 <aharipriya7093@gmail.com>
2024-04-21 11:00:36 +0530

    Practise programs are updated

---
+++ Practise/CommonCharacter.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CommonCharacter{
+
+public static int CommonCharactersCounter(String str1,String str2){
+  int count = 0;
+  HashMap<Character, Integer> charCountMap = new HashMap<>();
+
+  for (char ch : str1.toCharArray()) {
+      charCountMap.put(ch, charCountMap.getOrDefault(ch, 0) + 1);
+  }k
+  for (char ch : str2.toCharArray()) {
+      if (charCountMap.containsKey(ch)) {
+          count++;
+      }
+  }
+  return count;
+
+}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+      
+            String str1 = in.next();
+            String str2 = in.next();
+
+        System.out.println(CommonCharactersCounter(str1, str2));
+        in.close();
+        }
+    }
+
+
