9d95824425a10b98b1957312506b4c54333cb0ec
Haripriya987 <aharipriya7093@gmail.com>
2024-05-06 21:19:36 +0530

    Practise programs are updated

---
+++ Practise/ChocolatesCount .java
@@ -0,0 +1,24 @@
+public class ChocolatesCount {
+  {
+    public static int chocolateCount(int t,int c){
+        if(t<c)
+        return 0;
+    
+    return t/c;
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		while(n-->0){
+		    int t=in.nextInt();
+		    int c=in.nextInt();
+		    System.out.println(chocolateCount(t,c));
+		}
+
+	}
+}
+
+  
+}
