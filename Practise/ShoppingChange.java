08331841ebd7fe311671b2c0488ee9d9ad5fe73e
Haripriya987 <aharipriya7093@gmail.com>
2024-07-02 23:47:00 +0530

    Practise programs are submitted

---
+++ Practise/ShoppingChange.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ShoppingChange {
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		while(n-->0){
+		    int a=in.nextInt();
+		    System.out.println(100-a);
+		}
+
+	}
+}
+
+
