f3f9ace7ff4c1a34abce3ac2efbc3c9e72c4dee5
Haripriya987 <aharipriya7093@gmail.com>
2024-07-06 22:12:16 +0530

    Practise programs are submitted

---
+++ Practise/EqualCard.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		while(n-->0){
+		    int a=in.nextInt();
+		    System.out.println(52%a);
+		}
+
+	}
+}
