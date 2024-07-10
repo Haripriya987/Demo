3818f8c47367246c77e6585c836abd80acc82675
Haripriya987 <aharipriya7093@gmail.com>
2024-07-10 23:23:41 +0530

    practise programs are submitted

---
+++ Practise/Difference.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        while(n-->0){
+            int a=in.nextInt();
+            int b=in.nextInt();
+            System.out.println(a-b);
+        }
+	}
+}
