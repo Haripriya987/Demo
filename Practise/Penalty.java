cf6d51585cca77b9c7be5a3e67e6bfc90c46dbe8
Haripriya987 <aharipriya7093@gmail.com>
2024-07-12 23:19:04 +0530

    Practise programs are submitted

---
+++ Practise/Penalty.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Penalty
+{
+	public static void main (String[] args) 
+	{
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        
+        if(n>40)
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+	}
+}
