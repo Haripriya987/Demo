187a71fe9737a922611d8db025e6af579e5f9ce4
Haripriya987 <aharipriya7093@gmail.com>
2024-07-14 22:33:23 +0530

    Practise programs are submitted

---
+++ Practise/Target.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Target
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		while(n-->0){
+		    int a=in.nextInt();
+		    int b=in.nextInt();
+		    System.out.println(a-b);
+		}
+
+	}
+}
