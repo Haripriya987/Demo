b5767f4bc5adf011bba8a41eb53de18e06da461d
Haripriya987 <aharipriya7093@gmail.com>
2024-07-19 21:46:02 +0530

    Practise programs are submitted

---
+++ Practise/isGreater.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class isGreater
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int z=in.nextInt();
+		if(n>=z)
+		System.out.println("Yes");
+		else
+		System.out.println("No");
+
+	}
+}
