342b09ef3bab8f35db1e061f670caa0726fb40ca
Haripriya987 <aharipriya7093@gmail.com>
2024-07-20 23:28:19 +0530

    Practise programs are submitted

---
+++ Practise/CheckMinimum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class CheckMinimum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		if(n<60){
+		    System.out.println("No");
+		}
+		else{
+		    System.out.println("Yes");
+		}
+
+	}
+}
