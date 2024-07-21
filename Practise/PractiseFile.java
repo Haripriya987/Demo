7ff1a9c84c7131f8ac5bec19f98f2147262fe501
Haripriya987 <aharipriya7093@gmail.com>
2024-07-21 23:09:38 +0530

    Practise programs are submitted

---
+++ Practise/PractiseFile.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class PractiseFile
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int c=in.nextInt();
+		int x=in.nextInt();
+		if(a==x|| b==x||c==x){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+
+	}
+}
