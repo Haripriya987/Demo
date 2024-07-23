a32f52ecf076834550f689c5490361f3b905b6e8
Haripriya987 <aharipriya7093@gmail.com>
2024-07-23 22:54:17 +0530

    Practise programs are submitted

---
+++ Practise/Game.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Game
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		
+		    int a=in.nextInt();
+		    int b=in.nextInt();
+		    
+		    if(a>b)
+		    System.out.println("FREEKICK");
+		    else
+		    System.out.println("PENALTY");
+		
+
+	}
+}
