cb79794771f7d2cd970aa7540b2291a74e518044
Haripriya987 <aharipriya7093@gmail.com>
2024-07-30 22:51:42 +0530

    Practise programs are submitted

---
+++ Practise/Guess.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Guess
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		while(n-->0){
+		    int a=in.nextInt();
+		    System.out.println(7-a);
+		}
+
+	}
+}
