2d0e2f03d9a6d107d08fe9dd9aa692c2fc8c7b3f
Haripriya987 <aharipriya7093@gmail.com>
2024-08-09 22:09:40 +0530

    Practise programs are submitted

---
+++ Practise/Medals.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        int c=in.nextInt();
+        
+        System.out.println(15-a-b-c);
+        
+        
+	}
+}
