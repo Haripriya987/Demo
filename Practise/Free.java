199e120c96888df7cfe3f526482aeb31ed301a6a
Haripriya987 <aharipriya7093@gmail.com>
2024-08-23 22:42:33 +0530

    Practise programs are submitted

---
+++ Practise/Free.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        
+        System.out.println(n+(n/2));
+        
+	}
+}
