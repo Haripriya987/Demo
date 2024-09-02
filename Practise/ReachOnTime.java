05bd3f1a93c6828599e67fd0c29347c25b99a732
Haripriya987 <aharipriya7093@gmail.com>
2024-09-02 21:21:20 +0530

    Practise Programs are submitted

---
+++ Practise/ReachOnTime.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class ReachOnTime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		
+		while(n-->0){
+		  int z=in.nextInt();  
+		if(z>=30)
+		System.out.println("Yes");
+		else
+		System.out.println("No");
+		}
+
+	}
+}
