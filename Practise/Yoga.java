4e30b728540f551c6e13bbbde940109136b606af
Haripriya987 <aharipriya7093@gmail.com>
2024-09-06 23:10:38 +0530

    Practise programs are submitted

---
+++ Practise/Yoga.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Yoga
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        System.out.println(n/12);
+	}
+}
