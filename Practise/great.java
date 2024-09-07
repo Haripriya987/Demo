636d0df783ee2a9e10166af39bf3fd139f8c2ed3
Haripriya987 <aharipriya7093@gmail.com>
2024-09-07 21:40:51 +0530

    Practise programs are submitted

---
+++ Practise/great.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class great
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		if(n>30)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+
+	}
+}
